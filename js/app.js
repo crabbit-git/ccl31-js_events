4a058516e9b7cffd3437c12bf798fb5763e3aaeb
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-05-25 14:47:27 +0100

    Can now clear reading list

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
   
   const form = document.querySelector('#new-item-form');
   const categoryDefault = document.querySelector('#category>option[value=""]');
-  categoryDefault.textContent = 'Please select a category';
+  categoryDefault.textContent = 'Select document type';
   const readingList = document.querySelector('#reading-list');
+
   
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    // console.log(event);
-    const bookWrapper = document.createElement('li');
-    readingList.appendChild(bookWrapper);
+    const documentWrapper = document.createElement('li');
+    readingList.appendChild(documentWrapper);
     const submitResults = document.createElement('ul');
     readingList.appendChild(submitResults);
-
+    
     const bookTitle = document.createElement('li');
     bookTitle.textContent = event.target.title.value;
     submitResults.appendChild(bookTitle);
@@ -25,10 +25,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const bookCategory = document.createElement('li');
     bookCategory.textContent = event.target.category.value
     submitResults.appendChild(bookCategory);
-
+    
     form.reset();
   };
-
+  
   form.addEventListener('submit', handleFormSubmit);
+
+  const body = document.querySelector('body');
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Clear reading list';
+  body.appendChild(deleteButton);
+
+  const handleDeleteAll = () => {
+    while (readingList.lastChild) {
+      readingList.removeChild(readingList.lastChild);
+    }
+  };
+
+  deleteButton.addEventListener('click', handleDeleteAll);
 
 });
